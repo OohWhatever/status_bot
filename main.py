e6b30f5eb539ab80759f2c46a4ee4a91b9af5bf7
user <user@host.kalik>
2024-08-31 22:14:31 +0300

    updated embeed added status

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import random
+from datetime import datetime
 
 # Токен вашего бота (его нужно получить с Discord Developer Portal)
 TOKEN = ''
@@ -19,10 +20,45 @@ async def update_embed():
         if embed_message:
             # Создаем новое embed-сообщение с обновленным содержимым
             new_embed = discord.Embed(
-                title="Обновляемое сообщение",
-                description=f"Случайное число: {random.randint(1, 100)}",
-                color=discord.Color.blue()
+                title="STANDOFF365",
+                url="https://range.standoff365.com/",
+                description="Здесь вы можете проверить состояние отраслей на киберполигоне\n\n```\nПеречень отраслей\n```\n____",
+                colour=0xf50000,
+                timestamp=datetime.now()
             )
+
+            new_embed.set_author(
+                name="Cyberrange Status",
+                url="https://range.standoff365.com/",
+                icon_url="https://standoff365.com/favicons/icon.svg"
+            )
+
+            # Пример изменения статуса: случайное изменение статуса отрасли
+            statuses = ["🟢 Работает ","🟢 Работает ", "🟡 Наблюдаются проблемы","🔴 Сбой"]
+            new_embed.add_field(
+                name="> **Metal&Tech**",
+                value=f" {random.choice(statuses)} [Перейти](https://range.standoff365.com/battle/5/industry/17/) ",
+                inline=False
+            )
+            new_embed.add_field(
+                name="> **HighTechEnergy**",
+                value=f" {random.choice(statuses)} [Перейти](https://range.standoff365.com/battle/5/industry/38/) ",
+                inline=False
+            )
+            new_embed.add_field(
+                name="> **Банковская система**",
+                value=f" {random.choice(statuses)} [Перейти](https://range.standoff365.com/battle/5/industry/19/) ",
+                inline=False
+            )
+            new_embed.add_field(
+                name="> **NetFusionPro**",
+                value=f" {random.choice(statuses)} [Перейти](https://range.standoff365.com/battle/5/industry/18/) ",
+                inline=False
+            )
+
+            new_embed.set_image(url="https://standoff365.com/content/uploads/battle2_6c5484b198.png")
+            new_embed.set_footer(icon_url="https://standoff365.com/favicons/icon.svg")
+
             await embed_message.edit(embed=new_embed)
 
         # Ждем 30 секунд перед следующим обновлением
@@ -35,12 +71,49 @@ async def on_ready():
     # Получаем канал по его ID, где будет отправлено embed-сообщение
     channel = client.get_channel(1279509509135536330)
 
+    # Проверяем, найден ли канал
+    if channel is None:
+        print(f"Ошибка: Канал с ID {1279509509135536330} не найден. Проверьте ID канала и права доступа.")
+        return
+
     # Создаем первоначальное embed-сообщение
     embed = discord.Embed(
-        title="Обновляемое сообщение",
-        description="Это сообщение будет обновляться каждые 30 секунд.",
-        color=discord.Color.blue()
+        title="STANDOFF365",
+        url="https://range.standoff365.com/",
+        description="Здесь вы можете проверить состояние отраслей на киберполигоне\n\n```\nПеречень отраслей\n```\n____",
+        colour=0xf50000,
+        timestamp=datetime.now()
     )
+
+    embed.set_author(
+        name="Cyberrange Status",
+        url="https://range.standoff365.com/",
+        icon_url="https://standoff365.com/favicons/icon.svg"
+    )
+
+    embed.add_field(
+        name="> **Metal&Tech**",
+        value="✅ Металлургический комбинат [Metal&Tech](https://range.standoff365.com/battle/5/industry/17/) работает в штатном порядке",
+        inline=False
+    )
+    embed.add_field(
+        name="> **HighTechEnergy**",
+        value="✅ Отрасль электроэнергетики [HighTechEnergy](https://range.standoff365.com/battle/5/industry/38/) работает в штатном порядке",
+        inline=False
+    )
+    embed.add_field(
+        name="> **Банковская система**",
+        value="✅ [Банковская отрасль ](https://range.standoff365.com/battle/5/industry/19/) работает в штатном порядке",
+        inline=False
+    )
+    embed.add_field(
+        name="> **NetFusionPro**",
+        value="✅ IT-компания [NetFusionPro](https://range.standoff365.com/battle/5/industry/18/) работает в штатном порядке",
+        inline=False
+    )
+
+    embed.set_image(url="https://standoff365.com/content/uploads/battle2_6c5484b198.png")
+    embed.set_footer(icon_url="https://standoff365.com/favicons/icon.svg")
 
     # Отправляем embed-сообщение и сохраняем ссылку на него
     global embed_message
